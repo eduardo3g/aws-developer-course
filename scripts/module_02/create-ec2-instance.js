2b2748582632ec691d198a7277aa0b8b04d1641a
= <=>
2020-04-10 16:34:56 -0300

    feat: Creating an EC2 Instance

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -82,5 +82,23 @@ function createKeyPair(keyName) {
 }
 
 function createInstance(sgName, keyName) {
-  // TODO: create ec2 instance
+  const params = {
+    ImageId: 'ami-0323c3dd2da7fb37d',
+    InstanceType: 't2.micro',
+    KeyName: keyName,
+    MaxCount: 1,
+    MinCount: 1,
+    SecurityGroups: [
+      sgName
+    ],
+    // User data is the base64 encoding of ec2-startup.sh file
+    UserData: 'IyEvYmluL2Jhc2gNCmN1cmwgLS1zaWxlbnQgLS1sb2NhdGlvbiBodHRwczovL3JwbS5ub2Rlc291cmNlLmNvbS9zZXR1cF8xMi54IHwgc3VkbyBiYXNoIC0NCnN1ZG8geXVtIGluc3RhbGwgLXkgbm9kZWpzDQpzdWRvIHl1bSBpbnN0YWxsIC15IGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQNCmNkIGhiZmwNCm5wbSBpDQpucG0gcnVuIHN0YXJ0'
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.runInstances(params, (err, data) => {
+      if (err) reject(err);
+      else resolve(data);
+    });
+  });
 }
