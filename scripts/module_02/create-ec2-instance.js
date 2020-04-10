5d7bdb1bf461f9c0a981caab63a5a818f3632c40
= <=>
2020-04-10 15:40:33 -0300

    feat: Creating an EC2 Security Group

--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -1,40 +1,77 @@
-// Imports
-// TODO: Import the aws-sdk
+const AWS = require('aws-sdk');
 const helpers = require('./helpers')
 
-// TODO: Configure region
+AWS.config.update({ region: 'us-east-1' });
 
-// Declare local variables
-// TODO: Create an ec2 object
+const ec2 = new AWS.EC2();
 const sgName = 'hamster_sg'
 const keyName = 'hamster_key'
 
 // Do all the things together
 createSecurityGroup(sgName)
-.then(() => {
-  return createKeyPair(keyName)
-})
-.then(helpers.persistKeyPair)
-.then(() => {
-  return createInstance(sgName, keyName)
-})
-.then((data) => {
-  console.log('Created instance with:', data)
-})
-.catch((err) => {
-  console.error('Failed to create instance with:', err)
-})
+  .then(() => {
+    return createKeyPair(keyName)
+  })
+  .then(helpers.persistKeyPair)
+  .then(() => {
+    return createInstance(sgName, keyName)
+  })
+  .then((data) => {
+    console.log('Created instance with:', data)
+  })
+  .catch((err) => {
+    console.error('Failed to create instance with:', err)
+  })
 
-// Create functions
+function createSecurityGroup(sgName) {
+  const params = {
+    Description: sgName,
+    GroupName: sgName
+  }
 
-function createSecurityGroup (sgName) {
-  // TODO: Implement sg creation & setting SSH rule
+  return new Promise((resolve, reject) => {
+    ec2.createSecurityGroup(params, (err, data) => {
+      if (err) reject(err);
+      else {
+        const params = {
+          GroupId: data.GroupId,
+          IpPermissions: [
+            {
+              IpProtocol: 'tcp',
+              FromPort: 22, // Allow SSH on port 22 / From-To is a range of ports
+              ToPort: 22,
+              IpRanges: [
+                {
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            },
+            {
+              IpProtocol: 'tcp',
+              FromPort: 3000,
+              ToPort: 3000,
+              IpRanges: [
+                {
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            }
+          ]
+        }
+
+        ec2.authorizeSecurityGroupIngress(params, (err) => {
+          if (err) reject(err);
+          else resolve();
+        });
+      }
+    });
+  });
 }
 
-function createKeyPair (keyName) {
+function createKeyPair(keyName) {
   // TODO: Create keypair
 }
 
-function createInstance (sgName, keyName) {
+function createInstance(sgName, keyName) {
   // TODO: create ec2 instance
 }
