ac7a0692d7bab855f62812737dd2c70e6752d09c
= <=>
2020-04-13 20:58:53 -0300

    feat: Creating a DynamoDB table.

--- scripts/module_05/create-dynamo-table.js
+++ scripts/module_05/create-dynamo-table.js
@@ -1,19 +1,38 @@
-// Imports
-const AWS = require('aws-sdk')
+const AWS = require('aws-sdk');
 
-AWS.config.update({ region: '/* TODO: Add your region */' })
+AWS.config.update({ region: 'us-east-1' });
 
-// Declare local variables
-// TODO: Declare dynamoDB object
+const dynamo = new AWS.DynamoDB();
 
 createTable('hamsters')
 .then(() => createTable('races'))
 .then(data => console.log(data))
 
 function createTable (tableName) {
-  // TODO: Declare params for createTable
+  const params = {
+    TableName: tableName,
+    AttributeDefinitions: [ // Attributes used as our key
+      {
+        AttributeName: 'id',
+        AttributeType: 'N' // S - String, B - Binary, N - Number
+      }
+    ],
+    KeySchema: [ // Table keys based on the defined attributes
+      {
+        AttributeName: 'id',
+        KeyType: 'HASH'
+      }
+    ],
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 5,
+      WriteCapacityUnits: 5
+    }
+  };
 
   return new Promise((resolve, reject) => {
-    // TODO: Call createTable function
-  })
+    dynamo.createTable(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    });
+  });
 }
